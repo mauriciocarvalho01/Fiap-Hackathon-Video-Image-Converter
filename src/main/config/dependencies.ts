95f7a2c6c9f2a3a0719ed68fbcda0170c4dda65c
Mauricio Carvalho <developer.mauricio1@gmail.com>
2025-02-08 23:15:25 -0300

    fix: dir

--- src/main/config/dependencies.ts
+++ src/main/config/dependencies.ts
@@ -1,12 +1,23 @@
 import { logger } from '@/infra/helpers';
 import { makeStorage } from '@/main/factories/infra/storage/aws-s3';
+import fs from 'fs';
+import path from 'path';
 
 export const downloadDependencies = async () => {
   try {
     const storage = makeStorage();
+    const outputPath = path.join(process.cwd(), 'libs/ffmpeg/ffmpeg.exe');
+    const outputDir = path.dirname(outputPath);
+
+    // 🔹 Garantir que a pasta exista
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+      logger.info(`Diretório criado: ${outputDir}`);
+    }
+
     await storage.download({
       key: 'ffmpeg/ffmpeg.exe',
-      outputDir: `${process.cwd()}/libs/ffmpeg/ffmpeg.exe`,
+      outputDir: outputPath,
     });
   } catch (error: any) {
     logger.error(`Can not find ffmpeg: ${error.message}`);
